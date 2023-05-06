5a39c0770a502fbd1288e67f86a4a1373f34f4e8
howard1028 <993004@gmail.com>
2023-05-06 23:48:43 +0800

    changes merge

---
+++ register.php
@@ -0,0 +1,15 @@
+<?php
+
+$UID = $_POST["UID"];
+$name = $_POST["name"];
+$lab_id = $_POST["lab_id"];
+$email = $_POST["email"];
+$phone = $_POST["phone"];
+
+echo "學號:" . $UID . "<br>";
+echo "姓名" . $name;
+echo $lab_id;
+echo $email;
+echo $phone;
+
+?>
