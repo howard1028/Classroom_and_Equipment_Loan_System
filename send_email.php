7d68d88a98b71381e95531ac9d1b691ff53131b3
howard1028 <993004@gmail.com>
2023-05-15 01:06:07 +0800

    send email ok

--- send_email.php
+++ send_email.php
@@ -1,6 +1,3 @@
-
-
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,9 +6,11 @@
     <script type='text/javascript' src="https://cdn.jsdelivr.net/npm/@emailjs/browser@3/dist/email.min.js"></script>
 </head>
 <body>
+    <!-- PHP -->
     <?php
     // 測試寄信
     $conn = require_once "config.php";
+    session_start();
 
     $today = date("Y-m-d");
 
@@ -30,10 +29,48 @@
         $borrower_email = $email_row['Email'];
         
         // 判斷是否超過時間
-        // if ($borrow_date > $today){
+        if ($borrow_date < $today){
+            echo "<script>
+                // 初始化 EmailJS SDK，使用已建立的服務(service)的 ID
+                emailjs.init('Bgvw5npIQBXLchDZ-');
 
-        //     function_alert("成功寄出!");
-        // }
+                // 發送郵件
+                emailjs.send('service_z6r7xk4', 'template_fqy4fl8', {
+                    to_name: '$borrower_name',
+                    to_email: '$borrower_email',
+                    from_name: '{$_SESSION['lab']}',
+                    message: '提醒您：您有借用的教室及設備尚未歸還，請盡速歸還，謝謝。'
+                })
+                .then(function(response) {
+                    console.log('郵件已發送：', response);
+                }, 
+                function(error) {
+                    console.error('發生錯誤：', error);
+                });
+
+            </script>";
+
+
+
+            echo "<script>
+                // 初始化 EmailJS SDK，使用已建立的服務(service)的 ID
+                emailjs.init('Bgvw5npIQBXLchDZ-');
+
+                // 給系辦
+                emailjs.send('service_z6r7xk4','template_np0vkba', {
+                    subject: '教室借用超時通知',
+                    to_email: '{$_SESSION['email']}',
+                    message: '借用人學號：$borrower_id\n借用人姓名：$borrower_name\n借用時間：$borrow_date\n\n已超過歸還期限，請盡快處理。'
+                })
+                .then(function(response) {
+                    console.log('郵件已發送：', response);
+                }, 
+                function(error) {
+                    console.error('發生錯誤：', error);
+                });
+                
+            </script>";
+        }
     }
 
     // 跳出警示框顯示message，關閉警示框後跳轉回
@@ -48,31 +85,9 @@
     } 
 
     // 關閉資料庫連線
-    // mysqli_close($conn);
+    mysqli_close($conn);
     ?>
-
-
-    <script type='text/javascript'>
-        (function() {
-            // 初始化 EmailJS SDK，使用已建立的服務(service)的 ID
-            emailjs.init("Bgvw5npIQBXLchDZ-");
-
-            // 發送郵件
-            emailjs.send("service_z6r7xk4", "template_fqy4fl8", {
-                to_name: "<?php echo $borrower_name; ?>",
-                to_email: "<?php echo $borrower_email; ?>",
-                from_name: "系辦",
-                message: "您有借用的教室及設備尚未歸還，請盡速歸還!"
-            })
-            .then(function(response) {
-                console.log("郵件已發送：", response);
-            }, 
-            function(error) {
-                console.error("發生錯誤：", error);
-            });
-        })();
-    </script>
-
-    <h3 style='text-align: center'>Email發送成功!!</h3>
+    
+    <?php function_alert("Email發送成功!"); ?>
 </body>
 </html>
